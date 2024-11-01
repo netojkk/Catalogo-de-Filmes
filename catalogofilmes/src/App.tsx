583283cd7bb0d61e1b152b74788310a034df8479
neto <136371812+netojkk@users.noreply.github.com>
2024-11-01 15:48:35 -0300

    inicializando o template do form

--- catalogofilmes/src/App.tsx
+++ catalogofilmes/src/App.tsx
@@ -2,7 +2,16 @@ export function App() {
   
 
   return (
-   <h1>Hello, World</h1>
+   <main className="h-screen flex w-full">
+    <div className="bg-slate-800 w-full h-full flex p-16">
+      <section className="flex items-center justify-center h-screen">
+
+        <h1 className="">Qual Filme?</h1>
+        <input type="text" />
+      </section>
+
+    </div>
+   </main>
   )
 }
 
