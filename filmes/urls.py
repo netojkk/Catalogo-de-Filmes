ec9ccd35b6fbec80292b0e4acf5b1f13e6569974
neto <136371812+netojkk@users.noreply.github.com>
2024-11-01 10:42:39 -0300

    Protegendo a senha API KEY

--- filmes/urls.py
+++ filmes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, testar_api
 
 urlpatterns = [
     path('', home),
+    path('testar-api/', testar_api, name='testar-api')
 ]
