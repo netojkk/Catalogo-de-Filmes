a32fc8c6247c1bba6ff86128061af2ef2a073fb8
neto <136371812+netojkk@users.noreply.github.com>
2024-11-01 16:57:17 -0300

    Primeira interface criada
    Essa será a base da interface, ficará assim até o momento para conseguir testar a minha API estática e tentar a conexão entre back e front.

--- catalogofilmes/src/App.tsx
+++ catalogofilmes/src/App.tsx
@@ -1,13 +1,35 @@
+
+import { Button } from "./components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import { Search } from "lucide-react"
+import { Input } from "./components/ui/input";
+
 export function App() {
   
 
   return (
    <main className="h-screen flex w-full">
-    <div className="bg-slate-800 w-full h-full flex p-16">
-      <section className="flex items-center justify-center h-screen">
 
-        <h1 className="">Qual Filme?</h1>
-        <input type="text" />
+    <div className="bg-slate-600 w-full h-full flex">
+
+      <section className="flex items-center justify-center h-screen w-screen flex-col">
+        <Card>
+          <CardHeader className="tx">
+            <CardTitle className="text-4xl">
+              Pesquise o seu Filme
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center flex-row">
+              <Input type="text" placeholder="Digite um filme" className="text-lg mr-4 w-96"></Input>
+              
+              <Button type="submit">
+                <Search></Search>
+                Pesquisar
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
       </section>
 
     </div>
